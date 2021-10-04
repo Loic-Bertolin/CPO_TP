929fe202928a0403c460683ee2355dc3c6c0291a
loicb <loicb@192.168.115.115>
2021-10-04 15:18:22 +0200

    Finish

--- TP1_manipNombresInt/src/tp1_manipnombresint/TP1_manipNombresInt.java
+++ TP1_manipNombresInt/src/tp1_manipnombresint/TP1_manipNombresInt.java
@@ -4,17 +4,27 @@
  */
 package tp1_manipnombresint;
 
-/**
- *
- * @author loicb
+import java.util.Scanner;
+
+/*TP1 Syntaxe de base-manipNombreInt
+ *04/10/2021
+ *BERTOLIN Loïc
+ *TORDO Pierre
  */
 public class TP1_manipNombresInt {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Variable 1 : ");
+        int var1 = sc.nextInt();
+        System.out.println("Variable 2 : ");
+        int var2 = sc.nextInt();
+        System.out.println("Les nombres sont " + var1 + " et " + var2);
+        System.out.println("La somme est " + (var1 + var2));
+        System.out.println("La différence est " + (var1-var2));
+        System.out.println("Le produit est " + (var1*var2));
+        System.out.println("La division est " + (var1/var2));
+        System.out.println("Le quotient est " + (var1/var2) + "et le reste est" + (var1%var2));
     }
     
 }
