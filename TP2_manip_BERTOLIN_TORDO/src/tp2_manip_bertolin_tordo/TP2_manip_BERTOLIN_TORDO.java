117ac831683cbd4666577a0b31298b036787f99c
loicb <loicb@192.168.115.115>
2021-10-22 16:04:08 +0200

    debut

---
+++ TP2_manip_BERTOLIN_TORDO/src/tp2_manip_bertolin_tordo/TP2_manip_BERTOLIN_TORDO.java
@@ -0,0 +1,18 @@
+/*
+* TP2 manip
+* TORDO
+* BERTOLIN
+* 22/10/2021
+ */
+package tp2_manip_bertolin_tordo;
+
+/**
+ *
+ * @author loicb
+ */
+public class TP2_manip_BERTOLIN_TORDO {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
