fc94b5c6ec464015fe14c4aedddeef571ec1a58d
pierre <pierre@192.168.114.135>
2021-10-22 16:40:13 +0200

    Création exercice relation

---
+++ TP2_relation_1_BERTOLIN_TORDO/src/tp2_relation_1_bertolin_tordo/TP2_relation_1_BERTOLIN_TORDO.java
@@ -0,0 +1,22 @@
+/*
+* TP2 Biere
+* TORDO
+* BERTOLIN
+* 22/10/2021
+*/
+package tp2_relation_1_bertolin_tordo;
+
+/**
+ *
+ * @author pierre
+ */
+public class TP2_relation_1_BERTOLIN_TORDO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
