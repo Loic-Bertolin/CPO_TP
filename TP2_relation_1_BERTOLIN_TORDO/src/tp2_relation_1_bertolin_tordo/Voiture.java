c082c95d98d3599d5ecb5e7e06f66d161d0b232f
pierre <pierre@192.168.114.135>
2021-11-10 17:08:44 +0100

    Fini et commenté

--- TP2_relation_1_BERTOLIN_TORDO/src/tp2_relation_1_bertolin_tordo/Voiture.java
+++ TP2_relation_1_BERTOLIN_TORDO/src/tp2_relation_1_bertolin_tordo/Voiture.java
@@ -13,18 +13,19 @@ package tp2_relation_1_bertolin_tordo;
 public class Voiture {
     String Modele;
     String Marque;
+    Personne Proprietaire = null;
     int PuissanceCV;//on creer les variables
     
     public Voiture (String unModele, String uneMarque, int unePuissanceCV){
-        Modele = unModele;
+        Modele = unModele; // affection du nom à l'objet
         Marque = uneMarque;
         PuissanceCV = unePuissanceCV;
-        
+        //Proprietaire = this;
     }
     @Override
     public String toString() {
         String chaine;
-        chaine = "Modele " + Modele + "\n" + " Marque " + Marque + " PuissanceCV " + PuissanceCV;
+        chaine = "\n" +"Modele: " + Modele + "\n" + " Marque: " + Marque + "\n" +" PuissanceCV: " + PuissanceCV;
         return chaine;
     }
 }
