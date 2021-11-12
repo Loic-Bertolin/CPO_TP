58cca72c6761f781707a395b4161e3e4265cced3
pierre <pierre@192.168.114.135>
2021-11-12 18:49:00 +0100

    Grille 2

--- SP4_console_BERTOLIN_TORDO/src/sp4_console_bertolin_tordo/Grille.java
+++ SP4_console_BERTOLIN_TORDO/src/sp4_console_bertolin_tordo/Grille.java
@@ -12,7 +12,72 @@ public class Grille {
     Cellule[][]CellulesJeu = new Cellule[6][7];
     
     public Grille(){
+        for(int lignes=0;lignes<6;lignes++){
+            for(int colonnes= 0; colonnes<7; colonnes++){
+                CellulesJeu[lignes][colonnes]=null;
+            }
+        }
+    }
+    public boolean ajouterJetonDansColonne(Jeton unJeton, int unecolonne){
+        int lignes = 0;
+        do{
+            if(CellulesJeu[lignes][unecolonne]!=null){
+                lignes++;  
+            }
+        }while(CellulesJeu[lignes][unecolonne]!=null);
+        return CellulesJeu[lignes][unecolonne].affecterJeton(unJeton);
+    }
+    public boolean etreRemplie(){
+        for(int lignes=0;lignes<6;lignes++){
+            for(int colonnes= 0; colonnes<7; colonnes++){
+                if(CellulesJeu[lignes][colonnes]!=null){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public void viderGrille(){
+        for(int lignes=0;lignes<6;lignes++){
+            for(int colonnes= 0; colonnes<7; colonnes++){
+                CellulesJeu[lignes][colonnes]=null;  
+            }
+        }
+    }
+    public void afficherGrilleSurConsole(){
         
     }
+    public boolean celluleOccupee(int uneligne, int unecolonne){
+        return CellulesJeu[uneligne][unecolonne]!=null;
     
+    }
+    public String lireCouleurDuJeton(int uneligne, int unecolonne){
+        return CellulesJeu[uneligne][unecolonne].lireCouleurDuJeton();
+    }
+    public boolean etreGagnantePourJoueur(){
+        
+    }
+    public void tasserGrille(){ 
+        
+    }
+    public boolean colonneRemplie(int unecolonne){
+        for(int lignes= 0; lignes<6; lignes++){
+            if(CellulesJeu[lignes][unecolonne]==null){
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean placerTrouNoir(int uneligne, int unecolonne){
+        return CellulesJeu[uneligne][unecolonne].placerTrouNoir();   
+    }
+    public boolean placerDesintegrateur(int uneligne, int unecolonne){
+        return CellulesJeu[uneligne][unecolonne].placerDesintegrateur();
+    }
+    public boolean supprimerJeton(int uneligne, int unecolonne){
+        return CellulesJeu[uneligne][unecolonne].supprimerJeton();
+    }
+    public Jeton recupererJeton(int uneligne, int unecolonne){
+        return CellulesJeu[uneligne][unecolonne].recupererJeton();
+    }
 }
