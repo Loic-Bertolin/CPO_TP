7adb8237ef99da24918054e3b3553180afd7bf29
loicb <loicb@192.168.115.115>
2021-11-12 16:53:19 +0100

    Joueur

--- SP4_console_BERTOLIN_TORDO/src/sp4_console_bertolin_tordo/Joueur.java
+++ SP4_console_BERTOLIN_TORDO/src/sp4_console_bertolin_tordo/Joueur.java
@@ -9,5 +9,46 @@ package sp4_console_bertolin_tordo;
  * @author loicb
  */
 public class Joueur {
-    
+
+    String Nom;
+    String Couleur;
+    Jeton[] ListeJetons = new Jeton[21];
+    int nombreJetonRestants = 0;
+    int nombreDesintegrateurs = 0;
+
+    public void Joueur(String unNom) {
+        Nom = unNom;
+    }
+
+    public void affecterCouleur(String uneCouleur) {
+        Couleur = uneCouleur;
+    }
+
+    public boolean ajouterJeton(Jeton unJeton) {
+        int i = 0;
+        do {
+            if (ListeJetons[i] != null) {
+                i++;
+            if (i==21){
+                return false;
+            }
+            }
+        } while (ListeJetons[i] != null);
+        ListeJetons[i] = unJeton;
+        return true;
+    }
+
+    public int obtenirDesintegrateur() {
+        nombreDesintegrateurs++;
+        return nombreDesintegrateurs;
+    }
+    public boolean utiliserDesintegrateur() {
+        if (nombreDesintegrateurs==0){
+            return false;
+        }
+        else{
+            nombreDesintegrateurs=nombreDesintegrateurs-1;
+            return true;
+        }
+    }
 }
