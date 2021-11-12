0cb6a9a964bc481167b877bba0b511a01ea231cc
loicb <loicb@192.168.115.115>
2021-11-12 16:00:12 +0100

    Création

---
+++ SP4_console_BERTOLIN_TORDO/src/sp4_console_bertolin_tordo/SP4_console_BERTOLIN_TORDO.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sp4_console_bertolin_tordo;
+
+/**
+ *
+ * @author loicb
+ */
+public class SP4_console_BERTOLIN_TORDO {
+
+    /*SP4 console
+     *Loïc Bertolin
+     *Pierre Tordo
+     *12/11/21
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
